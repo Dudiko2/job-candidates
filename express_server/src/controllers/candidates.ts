f5aaf9d1a1c97619efb11ece839de35699403eb6
david <dudiko2@gmail.com>
2021-11-23 20:50:00 +0200

    react app TS, minor fix in server

--- express_server/src/controllers/candidates.ts
+++ express_server/src/controllers/candidates.ts
@@ -1,13 +1,13 @@
-import { StatusCodes } from "http-status-codes";
+import { ReasonPhrases, StatusCodes } from "http-status-codes";
 import { dbClient } from "../db";
 import type { RequestHandler } from "express";
 
 const getCandidatesController: RequestHandler = (req, res) => {
     dbClient.getCandidates(
-        (err) => {
+        () => {
             res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
                 success: false,
-                msg: err.message
+                msg: ReasonPhrases.INTERNAL_SERVER_ERROR
             });
         },
         (data) => {
